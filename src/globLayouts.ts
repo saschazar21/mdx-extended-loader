9ee7ba171402c2792ad09e72691bce5b9cffa1f7
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2019-12-14 17:03:25 +0100

    fix(props): fixed props handling of Frontmatter

---
+++ src/globLayouts.ts
@@ -0,0 +1,52 @@
+import globby from 'globby';
+import { basename, dirname, extname, relative, resolve } from 'path';
+import { loader } from 'webpack';
+
+import MDXLayoutLoaderOptions from 'interfaces/options';
+
+/**
+ * Filters the given layoutsDir-directory for possible React component layouts and returns a Map.
+ *
+ * @param this - The LoaderContext of webpack, needed for receiving the source file's URL and emitting error messages
+ * @param options - The options from the LoaderContext
+ */
+export default async function globLayouts(
+  this: loader.LoaderContext,
+  options: MDXLayoutLoaderOptions,
+): Promise<Map<string, string>> {
+  const { extensions, layoutsDir } = options;
+  const fileExtensions = Array.isArray(extensions) ? extensions : [extensions];
+  const absoluteLayoutsPath = resolve(process.cwd(), layoutsDir); // the absolute path to the layoutsDir
+  const relativeLayoutsPath = relative(process.cwd(), absoluteLayoutsPath); // the relative path from the CWD to the layoutsDir
+  const relativeResourcePath = relative(
+    dirname(this.resourcePath), // dirname() is necessary to cut off the filename from the path
+    absoluteLayoutsPath,
+  ); // the relative path from the source file to the layoutsDir
+  const results = new Map();
+
+  try {
+    // glob the relative layout path for files having one of the given extensions
+    const layouts = await globby(relativeLayoutsPath, {
+      expandDirectories: fileExtensions.map(extension => `*.${extension}`),
+    });
+
+    // when no layouts found, throw error
+    if (layouts.length === 0) {
+      throw new Error(`No layouts found in: ${absoluteLayoutsPath}`);
+    }
+
+    // for every layout file, filter its name and relative path to the source file, and add it to the Map
+    layouts.forEach((url: string) => {
+      const extension = extname(url);
+      const name = basename(url, extension);
+      const relativePath = `${relativeResourcePath}/${basename(url)}`;
+
+      return results.set(name, relativePath);
+    });
+  } catch (e) {
+    // whenever an error happened, print it, but don't stop compiling
+    this.emitError(e);
+  }
+
+  return results;
+}
--- src/loader.ts
+++ src/loader.ts
@@ -1,10 +1,9 @@
 import loaderUtils from 'loader-utils';
-import globby from 'globby';
 import matter from 'gray-matter';
-import { basename, extname, dirname, relative, resolve } from 'path';
 import { loader } from 'webpack';
 
 import MDXLayoutLoaderOptions from 'interfaces/options';
+import globLayouts from './globLayouts';
 
 /**
  * Wraps the content in a React component layout, together with the Frontmatter data as props.
@@ -17,61 +16,16 @@ function wrapContent(matter: any, content: string, layoutPath: string): string {
   // exclude any children data from the frontmatter, to not confuse the React component
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { children, ...props } = matter;
+  const exports = Object.keys(props).map(
+    key => `export const ${key} = ${JSON.stringify(props[key])};`,
+  );
 
   return `
 import Layout from './${layoutPath}';
-
-export default () => Layout(${JSON.stringify(props)});
-
+export default Layout;
+${exports.join('\n')}
 ${content}
   `;
-}
-
-/**
- * Filters the given layoutsDir-directory for possible React component layouts and returns a Map.
- *
- * @param this - The LoaderContext of webpack, needed for receiving the source file's URL and emitting error messages
- * @param options - The options from the LoaderContext
- */
-async function globLayouts(
-  this: loader.LoaderContext,
-  options: MDXLayoutLoaderOptions,
-): Promise<Map<string, string>> {
-  const { extensions, layoutsDir } = options;
-  const fileExtensions = Array.isArray(extensions) ? extensions : [extensions];
-  const absoluteLayoutsPath = resolve(process.cwd(), layoutsDir); // the absolute path to the layoutsDir
-  const relativeLayoutsPath = relative(process.cwd(), absoluteLayoutsPath); // the relative path from the CWD to the layoutsDir
-  const relativeResourcePath = relative(
-    dirname(this.resourcePath), // dirname() is necessary to cut off the filename from the path
-    absoluteLayoutsPath,
-  ); // the relative path from the source file to the layoutsDir
-  const results = new Map();
-
-  try {
-    // glob the relative layout path for files having one of the given extensions
-    const layouts = await globby(relativeLayoutsPath, {
-      expandDirectories: fileExtensions.map((extension) => `*.${extension}`),
-    });
-
-    // when no layouts found, throw error
-    if (layouts.length === 0) {
-      throw new Error(`No layouts found in: ${absoluteLayoutsPath}`);
-    }
-
-    // for every layout file, filter its name and relative path to the source file, and add it to the Map
-    layouts.forEach((url: string) => {
-      const extension = extname(url);
-      const name = basename(url, extension);
-      const relativePath = `${relativeResourcePath}/${basename(url)}`;
-
-      return results.set(name, relativePath);
-    });
-  } catch (e) {
-    // whenever an error happened, print it, but don't stop compiling
-    this.emitError(e);
-  }
-
-  return results;
 }
 
 /**
